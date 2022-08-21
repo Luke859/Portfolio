efc114bfcce2461df05d4adf37af24ded27a9fae
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-08-21 18:20:51 +0200

    ajout images, articles

--- src/components/Articles/Projets.vue
+++ src/components/Articles/Projets.vue
@@ -44,7 +44,7 @@
                 <p></p>
                 <br>
                 <p>Voici le résultat final de la tâche</p>
-                <img id="photo2Tache1" src="../../assets/Tache3Fin.png" />
+                <!-- <img id="photo2Tache1" src="../../assets/Tache3Fin.png" /> -->
                 <br>
             </div>
             <div id="mapid" ></div>
